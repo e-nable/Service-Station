a510afd3bbf4eb1aa2980f228a21aa8ed8db7b42
creuzerm <git@creuzer.com>
2014-04-09 17:42:56 -0500

    Added right side parameters

--- index.php
+++ index.php
@@ -18,12 +18,13 @@
  session_start();
 $userid = session_id();
 
-echo "<p>This is a demonstration file generator. Reference documentation can be found <a href='https://docs.google.com/document/d/1LX3tBpio-6IsMMo3aaUdR-mLwWdv1jS4ooeEHb79JYo/edit?pli=1' target='_blank'>here</a>. Generator code can be found on <a href='https://github.com/laird/e-NABLE' target='_blank'>GitHub</a></p>";
+echo "<p>This is a demonstration file generator. Reference documentation can be found <a href='https://docs.google.com/document/d/1LX3tBpio-6IsMMo3aaUdR-mLwWdv1jS4ooeEHb79JYo/edit?pli=1' target='_blank'>here</a>. Generator code can be found on <a href='https://github.com/laird/e-NABLE' target='_blank'>GitHub</a> and the code for this web interface can be found an <a href='https://github.com/creuzerm/e-NABLE-Web-Generator' target='_blank'>GitHub</a>.</p>";
 
 
 echo <<<HTML
 <h1>Measurements</h1>
 <form>
+<fieldset>
   <legend>Left arm</legend>
    1 <input type="number" step="any" min="0" name="Left1" value='{$_REQUEST['Left1']}'  placeholder="Length of Elbow Joint"><br>
    2 <input type="number" step="any" min="0" name="Left2" value='{$_REQUEST['Left2']}'  placeholder="Distance between lateral and medial side of the forearm proximal to the elbow joint"><br>
@@ -36,6 +37,21 @@ echo <<<HTML
    9 <input type="number" step="any" min="0" name="Left9" value='{$_REQUEST['Left9']}'  placeholder="Distance from wrist to distal end on thumb side (Medial)"><br>
   10<input type="number" step="any" min="0" name="Left10" value='{$_REQUEST['Left10']}'  placeholder="Length of Elbow to wrist joint"><br>
   <input type="submit" name='submit' value="Preview">
+</fieldset>
+<fieldset>
+  <legend>Right arm</legend>
+   1 <input type="number" step="any" min="0" name="Right1" value='{$_REQUEST['Right1']}'  placeholder="Length of Elbow Joint"><br>
+   2 <input type="number" step="any" min="0" name="Right2" value='{$_REQUEST['Right2']}'  placeholder="Distance between lateral and medial side of the forearm proximal to the elbow joint"><br>
+   3 <input type="number" step="any" min="0" name="Right3" value='{$_REQUEST['Right3']}'  placeholder="Distance between lateral and medial side of the middle forearm"><br>
+   4 <input type="number" step="any" min="0" name="Right4" value='{$_REQUEST['Right4']}'  placeholder="Distance between lateral and medial side of the forearm proximal to the wrist"><br>
+   5 <input type="number" step="any" min="0" name="Right5" value='{$_REQUEST['Right5']}'  placeholder="Wrist Joint distance from lateral to medial side"><br>
+   6 <input type="number" step="any" min="0" name="Right6" value='{$_REQUEST['Right6']}'  placeholder="Distance from wrist to distal end on thumb side (Lateral)"><br>
+   7 <input type="number" step="any" min="0" name="Right7" value='{$_REQUEST['Right7']}'  placeholder="Distance from wrist to distal middle end of effected hand"><br>
+   8 <input type="number" step="any" min="0" name="Right8" value='{$_REQUEST['Right8']}'  placeholder="Distance from Lateral and Medial sides of the distal part of the hand"><br>
+   9 <input type="number" step="any" min="0" name="Right9" value='{$_REQUEST['Right9']}'  placeholder="Distance from wrist to distal end on thumb side (Medial)"><br>
+  10<input type="number" step="any" min="0" name="Right10" value='{$_REQUEST['Right10']}'  placeholder="Length of Elbow to wrist joint"><br>
+</fieldset>
+  <input type="submit" name='submit' value="Preview">
 </form>
 HTML;
 
@@ -44,8 +60,10 @@ HTML;
 
 if(isset($_REQUEST['submit']) && $_REQUEST['submit'] == 'Preview')
 {
-        $assemblypath = "e-NABLE/Assembly/";
-        $command = " openscad -o imagecache/{$userid}preview.png -D Left1={$_REQUEST['Left1']} -D Left2={$_REQUEST['Left2']} -D  Left3={$_REQUEST['Left3']} -D  Left4={$_REQUEST['Left4']} -D  Left5={$_REQUEST['Left5']} -D  Left6={$_REQUEST['Left6']} -D  Left7={$_REQUEST['Left7']} -D  Left8={$_REQUEST['Left8']} -D  Left9={$_REQUEST['Left9']} -D  Left10={$_REQUEST['Left10']}  {$assemblypath}Assembly.scad ";
+	$assemblypath = "e-NABLE/Assembly/";
+	$leftsidevars = "-D Left1={$_REQUEST['Left1']} -D Left2={$_REQUEST['Left2']} -D  Left3={$_REQUEST['Left3']} -D  Left4={$_REQUEST['Left4']} -D  Left5={$_REQUEST['Left5']} -D  Left6={$_REQUEST['Left6']} -D  Left7={$_REQUEST['Left7']} -D  Left8={$_REQUEST['Left8']} -D  Left9={$_REQUEST['Left9']} -D  Left10={$_REQUEST['Left10']}";
+	$rightsidevars = "-D Right1={$_REQUEST['Right1']} -D Right2={$_REQUEST['Right2']} -D  Right3={$_REQUEST['Right3']} -D  Right4={$_REQUEST['Right4']} -D  Right5={$_REQUEST['Right5']} -D  Right6={$_REQUEST['Right6']} -D  Right7={$_REQUEST['Right7']} -D  Right8={$_REQUEST['Right8']} -D  Right9={$_REQUEST['Right9']} -D  Right10={$_REQUEST['Right10']}";
+        $command = " openscad -o imagecache/{$userid}preview.png {$leftsidevars} {$rightsidevars} {$assemblypath}Assembly.scad ";
         echo "<p>{$command}</p>\n";
 
         $time_start = microtime(true);
