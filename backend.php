6667e8cb4ceec2407611c6857ac6aac66783bc3e
root <root@DysTopia>
2014-04-24 21:27:05 -0500

    Fix for bad preview image.

--- backend.php
+++ backend.php
@@ -21,8 +21,15 @@ function render()
 $return = '';
 if(isset($_REQUEST['submit']) )
 {
-	$previewimage = "imagecache/{$userid}preview.png";
-	$exportfile = "imagecache/{$userid}.stl";
+	$assemblypath = "../e-NABLE/Assembly/";
+	$leftsidevars = "-D Left1={$_REQUEST['Left1']} -D Left2={$_REQUEST['Left2']} -D  Left3={$_REQUEST['Left3']} -D  Left4={$_REQUEST['Left4']} -D  Left5={$_REQUEST['Left5']} -D  Left6={$_REQUEST['Left6']} -D  Left7={$_REQUEST['Left7']} -D  Left8={$_REQUEST['Left8']} -D  Left9={$_REQUEST['Left9']} -D  Left10={$_REQUEST['Left10']}";
+	$rightsidevars = "-D Right1={$_REQUEST['Right1']} -D Right2={$_REQUEST['Right2']} -D  Right3={$_REQUEST['Right3']} -D  Right4={$_REQUEST['Right4']} -D  Right5={$_REQUEST['Right5']} -D  Right6={$_REQUEST['Right6']} -D  Right7={$_REQUEST['Right7']} -D  Right8={$_REQUEST['Right8']} -D  Right9={$_REQUEST['Right9']} -D  Right10={$_REQUEST['Right10']}";
+	$options = " -D part={$_REQUEST['part']} -D fingerSelect={$_REQUEST['fingerSelect']} -D palmSelect={$_REQUEST['palmSelect']} -D WristBolt={$_REQUEST['WristBolt']} -D KnuckleBolt={$_REQUEST['KnuckleBolt']} -D JointBolt={$_REQUEST['JointBolt']} -D ThumbBolt={$_REQUEST['ThumbBolt']}  ";
+
+	$scalehash = md5($leftsidevars.$rightsidevars.$options) .'.'. crc32($leftsidevars.$rightsidevars.$options);
+
+	$previewimage = "imagecache/{$scalehash}.png";
+	$exportfile   = "imagecache/{$scalehash}.stl";
 
 	if($_REQUEST['submit'] == 'stl')
 	{
@@ -30,31 +37,31 @@ if(isset($_REQUEST['submit']) )
 		$downloadlink = "<p><a href='{$exportfile}'>Download .stl file.</a></p>\n";
 	}else{
 		$thingtodo = $previewimage;
+		$downloadlink = '';
 	}
 
-	$assemblypath = "e-NABLE/Assembly/";
-	$leftsidevars = "-D Left1={$_REQUEST['Left1']} -D Left2={$_REQUEST['Left2']} -D  Left3={$_REQUEST['Left3']} -D  Left4={$_REQUEST['Left4']} -D  Left5={$_REQUEST['Left5']} -D  Left6={$_REQUEST['Left6']} -D  Left7={$_REQUEST['Left7']} -D  Left8={$_REQUEST['Left8']} -D  Left9={$_REQUEST['Left9']} -D  Left10={$_REQUEST['Left10']}";
-	$rightsidevars = "-D Right1={$_REQUEST['Right1']} -D Right2={$_REQUEST['Right2']} -D  Right3={$_REQUEST['Right3']} -D  Right4={$_REQUEST['Right4']} -D  Right5={$_REQUEST['Right5']} -D  Right6={$_REQUEST['Right6']} -D  Right7={$_REQUEST['Right7']} -D  Right8={$_REQUEST['Right8']} -D  Right9={$_REQUEST['Right9']} -D  Right10={$_REQUEST['Right10']}";
-	$command = " openscad -o {$thingtodo} {$leftsidevars} {$rightsidevars} -D  part={$_REQUEST['part']} -D fingerSelect={$_REQUEST['fingerSelect']} -D palmSelect={$_REQUEST['palmSelect']} -D WristBolt={$_REQUEST['WristBolt']} -D KnuckleBolt={$_REQUEST['KnuckleBolt']} -D JointBolt={$_REQUEST['JointBolt']} -D ThumbBolt={$_REQUEST['ThumbBolt']} {$assemblypath}Assembly.scad ";
+	$command = " openscad -o {$thingtodo} {$leftsidevars} {$rightsidevars} {$options} {$assemblypath}Assembly.scad ";
 
-	$time_start = microtime(true);
-	$results = exec( "export DISPLAY=:5; " . escapeshellcmd($command));
-	$time_end = microtime(true);
-	$execution_time = ($time_end - $time_start);
+	// Lets do some disk caching. If we have already rendered this, lets use the pre-rendering
+	if(!file_exists($thingtodo))
+	{
+		$time_start = microtime(true);
+		$results = exec( "export DISPLAY=:5; " . escapeshellcmd($command));
+		$time_end = microtime(true);
+		$execution_time = ($time_end - $time_start);
+	}
+	$return =  "<img src='imagecache/{$scalehash}.png' style='width:100%;' /> {$downloadlink}";
+
+	//	echo "<input type='submit' name='submit' value='Create .STL'> {$downloadlink}";
+	//	echo "<p>Created preview in {$execution_time} seconds using the following command.</p>\n";
+	$return .= "<p style='color:ivory;'>{$command}</p>\n";
 
 
-	$return =  "<img src='imagecache/{$userid}preview.png' style='width:100%;' /> {$downloadlink}";
-
-//	echo "<input type='submit' name='submit' value='Create .STL'> {$downloadlink}";
-//	echo "<p>Created preview in {$execution_time} seconds using the following command.</p>\n";
-//	echo "<p>{$command}</p>\n";
-
-
-//	echo "<p>Computer Stats - for performance considerations</p>\n";
-//	echo "<pre>\n";
-//	echo `cat /proc/cpuinfo`;
-//	echo `cat /proc/meminfo`;
-//	echo "</pre>";
+	//	echo "<p>Computer Stats - for performance considerations</p>\n";
+	//	echo "<pre>\n";
+	//	echo `cat /proc/cpuinfo`;
+	//	echo `cat /proc/meminfo`;
+	//	echo "</pre>";
 }else
 {
 	$return = "<p>A sample data set can be loaded by <a href='?Left1=66.47&Left2=64.04&Left3=46.95&Left4=35.14&Left5=35.97&Left6=27.27&Left7=31.80&Left8=40.97&Left9=31.06&Left10=147.5&Right1=62.67&Right2=65.62&Right3=59.14&Right4=48.78&Right5=51.85&Right6=16.4&Right7=0&Right8=72.52&Right9=72.23&Right10=230.6&part=0&fingerSelect=1&palmSelect=1&WristBolt=5.5&KnuckleBolt=3.3&JointBolt=3.3&ThumbBolt=3.3&submit=Preview'>Clicking here</a>.</p>\n";
