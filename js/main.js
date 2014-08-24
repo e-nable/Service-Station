f21e0afec596fc2b7dd01af9422b289c0e3cd532
Ro Ortiz <rortizze@yahoo.com>
2014-08-24 14:38:48 -0700

    License details

--- js/main.js
+++ js/main.js
@@ -1,3 +1,21 @@
+/*  Web interface for back-end e-NABLE Assembler
+
+    Copyright (C) 2014, e-NABLE / Rogelio Ortiz, Mike Creuzer
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see http://www.gnu.org/licenses/.
+    */
+
 var submitVal = 0;
 var flaggedError = true;
 var isSimple = true;
