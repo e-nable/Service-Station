edd783e25777e3db36c64822f30c0f2176f30ba1
Aleks Jones <aleksjones@gmail.com>
2014-10-20 13:54:36 -0500

    Analytics enhancements and deleted deprecated JavaScript stuff.

--- index.php
+++ index.php
@@ -47,7 +47,23 @@ $sessionID = getSessionId();
 	<script src="./lib/sammy-latest.min.js"></script>
 	
 	<?php echo file_get_contents('js/knockout_templates.html'); ?>
-	<script src="./js/main.js"></script> <!-- ?_=<?php echo $time ?>"></script> DISABLE THIS FOR NOW TO MAKE DEBUGGING EASY IN CHROME-->
+	
+	<script type="text/javascript">
+		// TODO: move this stuff into AJAX services, etc.
+		var server_email = function() { $("#email").val(); };
+		var server_paddingValue = function() { $("#paddingValue").val(); };
+		
+		// Google Analytics
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js', 'ga');
+		
+		/* ga('create', 'UA-55621004-1', { 'cookieDomain': 'none' });	// FOR DEBUGGING PURPOSES ON LOCAL*/
+		ga('create', 'UA-55621004-1', 'auto');
+		ga('send', 'pageview', window.location.path);
+	</script>
+	<script src="./js/main.js?_=<?php echo $time ?>"></script> <!-- REMOVE THE PHP ECHO TO MAKE DEBUGGING EASIER IN CHROME-->
 	
 	<script type="text/javascript">
 		// TODO: move this stuff into AJAX services, etc.
