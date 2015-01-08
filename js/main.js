e7536df653fa1eb8f81cbadbfa8051db66d46a28
Ro Ortiz <rortizze@yahoo.com>
2015-01-08 01:17:31 -0800

    Adding description details to catalog items

--- js/main.js
+++ js/main.js
@@ -414,4 +414,24 @@ var viewModel = function (descriptionData) {
 	});
 };
 
+ko.bindingHandlers.trimLengthText = {};
+ko.bindingHandlers.trimText = {
+    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+        var trimmedText = ko.computed(function () {
+            var untrimmedText = ko.utils.unwrapObservable(valueAccessor());
+            var defaultMaxLength = 220;
+            var minLength = 5;
+            var maxLength = ko.utils.unwrapObservable(allBindingsAccessor().trimTextLength) || defaultMaxLength;
+            if (maxLength < minLength) maxLength = minLength;
+            var text = untrimmedText.length > maxLength ? untrimmedText.substring(0, maxLength - 1) + '...' : untrimmedText;
+            return text;
+        });
+        ko.applyBindingsToNode(element, {
+            text: trimmedText
+        }, viewModel);
 
+        return {
+            controlsDescendantBindings: true
+        };
+    }
+};
